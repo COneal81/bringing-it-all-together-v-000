5704c6b12d4f10959804d3bec4db1d14534f1588
Kari O'Neal <triadfirewood@gmail.com>
2019-06-09 22:37:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -73,10 +73,10 @@ class Dog
   end
   
   def self.find_by_name(name)
-    sql =<<-SQL
+    sql = <<-SQL
       SELECT * FROM dogs 
       WHERE name = ?
-      LIMIT 1
+      
       SQL
     DB[:conn].execute(sql, name).map do|row|
       self.new_from_db(row)
