30d96e97bef7e795de487db9ddca2c934b33a317
Kari O'Neal <triadfirewood@gmail.com>
2019-06-09 22:11:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,6 +72,7 @@ class Dog
       WHERE name = ?
       LIMIT 1
       SQL
-    DB[:conn].execute(sql, name)
-  end 
+    DB[:conn].execute(sql, name).map do|row|
+    self.new_from_db(row)
+  end.first
 end 
